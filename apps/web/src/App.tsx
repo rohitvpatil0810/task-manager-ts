a3d0a584974f8e930536ce085808adccf4e89ce1
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-23 22:50:38 +0530

    feat(web): setup vite react, tailwind css, shadcn ui - web project

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Project Setup</Button>
+    </>
+  );
+}
+
+export default App;
