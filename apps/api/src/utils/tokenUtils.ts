60c8c0c7722c1149b13a2494947e84b7b9d7a5a4
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-24 19:25:36 +0530

    feat(api): add generateToken and verifyToken

---
+++ apps/api/src/utils/tokenUtils.ts
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken';
+import prisma from './prismaClient';
+import { AuthFailureError } from '../core/ApiError';
+import { INVALID_TOKEN, USER_NOT_FOUND } from '../constants/errorMessages';
+import Logger from '../core/Logger';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
+
+export const generateToken = (userId: string) => {
+  return jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: '8hr',
+  });
+};
+
+export const verifyToken = async (token: string) => {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decoded.userId,
+      },
+    });
+    if (!user) throw new AuthFailureError(USER_NOT_FOUND);
+
+    if (
+      user.lastLogout &&
+      decoded.iat &&
+      decoded.iat < user.lastLogout.getTime()
+    ) {
+      throw new AuthFailureError(INVALID_TOKEN);
+    }
+
+    return user;
+  } catch (error) {
+    Logger.error('Error in verifyToken utils:', error);
+    if (
+      error instanceof jwt.TokenExpiredError ||
+      error instanceof jwt.JsonWebTokenError
+    ) {
+      throw new AuthFailureError(INVALID_TOKEN);
+    } else {
+      throw error;
+    }
+  }
+};
