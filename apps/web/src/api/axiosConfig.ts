2918cbb84cce78afca9211e90a2c3ad8e88bfc21
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-25 23:48:09 +0530

    feat(web): add axiosConfig.ts

---
+++ apps/web/src/api/axiosConfig.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (error.response.data.statusCode === 10003) {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
