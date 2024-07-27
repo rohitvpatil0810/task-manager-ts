a79dbaed5336eb7999844853a1581d4ee000374a
Rohit Patil <rohitvpatil0810@gmail.com>
2024-07-27 22:59:14 +0530

    chore(api): add trim to validations

--- apps/api/src/validations/authValidation.ts
+++ apps/api/src/validations/authValidation.ts
@@ -1,18 +1,18 @@
 import Joi from 'joi';
 
 export const signUpValidation = Joi.object({
-  firstName: Joi.string().required().min(3).max(30).lowercase(),
-  lastName: Joi.string().required().min(3).max(30).lowercase(),
-  email: Joi.string().email().required().lowercase(),
-  password: Joi.string().required().min(6).max(30),
-  confirmPassword: Joi.string().required().valid(Joi.ref('password')),
+  firstName: Joi.string().required().min(3).max(30).lowercase().trim(),
+  lastName: Joi.string().required().min(3).max(30).lowercase().trim(),
+  email: Joi.string().email().required().lowercase().trim(),
+  password: Joi.string().required().min(6).max(30).trim(),
+  confirmPassword: Joi.string().required().valid(Joi.ref('password')).trim(),
 });
 
 export const loginValidation = Joi.object({
-  email: Joi.string().email().required().lowercase(),
-  password: Joi.string().required(),
+  email: Joi.string().email().required().lowercase().trim(),
+  password: Joi.string().required().trim(),
 });
 
 export const googleSigninValidation = Joi.object({
-  tokenId: Joi.string().required(),
+  tokenId: Joi.string().required().trim(),
 });
